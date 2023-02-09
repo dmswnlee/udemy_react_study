2cf01c7ab4c8fda59a161fb0ebaa677a6a712b16
dmswnlee <dlrmawnl03@gmail.com>
2023-02-09 16:37:52 +0900

    간단한 리액트 프로젝트

---
+++ react-projet/src/components/UI/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Card.module.css';
+
+export default function Card(props) {
+    return (
+        //<div className={classes.card}>{props.children}</div>
+        <div className={`${classes.card} ${props.className}`}>
+            {props.children}
+        </div>
+    );
+}
+
